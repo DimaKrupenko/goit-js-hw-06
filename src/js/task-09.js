8379fd97d28cffc98867ac1bb0d1b1a38d814cfd
DimaKrupenko <dimakrupenko77@gmail.com>
2022-09-29 16:20:01 +0300

    сделал 9 задание

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector("span");
+
+button.addEventListener("click", onButton);
+
+function onButton(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
